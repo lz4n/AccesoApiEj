194f9fb3423d93d913c440a8f793c81c3bf08cb4
Izan <70977582+lz4n@users.noreply.github.com>
2024-05-09 20:25:37 +0200

    Add files via upload

---
+++ PeticionApi/Model/Pet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeticionApi.Model
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ PeticionApi/Program.cs
@@ -0,0 +1,49 @@
+﻿using PeticionApi.Model;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+
+namespace PeticionApi
+{
+    internal class Program
+    {
+        private static string ApiURI = "https://petstore.swagger.io/v2/", PostPetEndpoint = "pet";
+
+        static async Task Main(string[] args)
+        {
+            using (HttpClient client = new() { BaseAddress = new Uri(ApiURI) })
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.PutAsJsonAsync(PostPetEndpoint, new Pet { Id = 0, Name = "DOGGIE" });
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("La mascota se ha insertado correctamente: ");
+                        Console.WriteLine(await response.Content.ReadAsStringAsync());
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Ha ocurrido un error insertando la mascota: {response.StatusCode}");
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"Error en la solicitud HTTP: {ex.Message}");
+                }
+                catch (JsonException ex)
+                {
+                    Console.WriteLine($"Error en la manipulación de JSON: {ex.Message}");
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine($"Operación no válida: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Se ha producido una excepción no controlada: {ex.Message}");
+                }
+            }
+        }
+    }
+}
